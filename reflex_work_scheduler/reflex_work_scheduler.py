6c1c54bc020ba6e31e7daf7dc746bc4114ddb220
apizarro18 <pizarroalexander18@yahoo.com>
2025-12-21 14:38:01 -0600

    fixed the issue of shifts not being added to the table, moved from buttons to checkboxes, and added 12-hour format for easier readability

--- reflex_work_scheduler/reflex_work_scheduler.py
+++ reflex_work_scheduler/reflex_work_scheduler.py
@@ -1,7 +1,7 @@
 """Welcome to Reflex! This file outlines the steps to create a basic app."""
 
 import reflex as rx
-from states import employeeState
+from states import employeeState, availabilityState
 from pages.index import index
 from pages.availability import availability
 from typing import List
@@ -21,4 +21,4 @@ app = rx.App(
     )
 )
 app.add_page(index, on_load=employeeState.get_employees)
-app.add_page(availability, route="/availability")
+app.add_page(availability, route="/availability", on_load=availabilityState.load_employee_name(1))
